4b006406049497cf0da2ae9d1da11dde4bcef4d9
QuickGandharv <gandharvakalia@gmail.com>
2026-03-27 13:28:04 +0530

    update profile link in header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -434,7 +434,7 @@ export function Header() {
 
                     <DropdownMenuGroup>
                       <DropdownMenuItem asChild>
-                        <Link href="/doctor/profile" className="cursor-pointer">
+                        <Link href="/profile" className="cursor-pointer">
                           <UserIcon className="mr-2 h-4 w-4" />
                           <span>Profile</span>
                         </Link>
