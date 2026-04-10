a09e4ee42cb3b5802e0ef43f079e2fda9d1baa90
QuickGandharv <gandharvakalia@gmail.com>
2026-04-10 18:30:10 +0530

    add margin in add prescription button

--- app/(main)/appointments/detail-component/PrescriptionTab.tsx
+++ app/(main)/appointments/detail-component/PrescriptionTab.tsx
@@ -134,7 +134,7 @@ export default function PrescriptionTab({ appointmentId }: { appointmentId: stri
     if (!medicines.length && !instructionsByDoctor && !nextVisitDate) {
         return (
             <>
-                <div className="flex justify-end my-4">
+                <div className="flex justify-end mt-4 mb-4">
                     <Button
                         onClick={() => setIsAddDialogOpen(true)}
                         className="h-8 sm:h-9 text-xs sm:text-sm"
